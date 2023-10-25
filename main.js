ae2228fc13f39fa9c6dd19889a273c75f05bdc38
elishamutang <elishamutang@outlook.com>
2023-10-25 21:28:52 +0800

    Drafting.

--- main.js
+++ main.js
@@ -6,7 +6,17 @@ const createAcc = document.querySelector("button");
 pwd.addEventListener("change", doSomething);
 pwdConfirm.addEventListener("change", doSomething);
 
+pwd.addEventListener("input", doSomething);
+pwdConfirm.addEventListener("input", doSomething);
+
+
 function doSomething(e) {
     console.log(e.target.value);
     console.log(pwdConfirm.value);
+
+    if(pwd.value == pwdConfirm.value) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }
 }
