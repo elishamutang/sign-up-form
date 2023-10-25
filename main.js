59c936ca974eb9ddc2d87d52af34e725a48e167f
elishamutang <elishamutang@outlook.com>
2023-10-25 21:25:07 +0800

    Initial draft.

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const pwdConfirm = document.getElementById("userPwdConfirm");
 const createAcc = document.querySelector("button");
 
 pwd.addEventListener("change", doSomething);
+pwdConfirm.addEventListener("change", doSomething);
 
 function doSomething(e) {
     console.log(e.target.value);
+    console.log(pwdConfirm.value);
 }
